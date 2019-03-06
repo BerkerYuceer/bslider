6dfe5df4496394b48bca2a104c5f66cd130f3acf
Berker Yüceer <berkeryuceer@yahoo.com>
2019-03-06 10:44:47 +0300

    Update jquery-bshowcase.js
    test

--- jquery-bshowcase.js
+++ jquery-bshowcase.js
@@ -20,7 +20,7 @@
 * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
 * OTHER DEALINGS IN THE SOFTWARE.
 */
-;(function ($, undefined) {
+(function ($) {
     $.widget("fn.bshowcase", {
         options: {
             width: 0,
